1c86be8a9e4225fec9926699f0bb827fc28b2257
Peter Wang <happypeter1983@gmail.com>
2015-10-26 12:11:29 +0800

    webpack sass

--- main.js
+++ main.js
@@ -2,5 +2,7 @@ var App = require("./components/App.js");
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+require('./styles/main.scss');
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
